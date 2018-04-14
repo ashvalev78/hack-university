8aff700f44d53f54a92c41fe77183b174cb0a377
Aleksandr <ashvalev78@gmail.com>
2018-04-14 11:15:06 +0300

    mapAdded

---
+++ src/components/map.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id = "mapContainer">
+
+    </div>
+</template>
+
+<script>
+    var platform = new H.servive.Platform({
+        'app_id': 'y0fwQjbIby4yR6IpY7JO',
+        'app_code': '0z6_tBhCaH41eAbiiTDbVw'
+    });
+
+    var mapLayers = platform.createDefaultLayers();
+
+    var map = new H.map(
+        document.getElementById('mapContainer'),
+        mapLayers.normal.map,
+        {
+            zoom: 10,
+            center: {
+                lat: 60.000973, lng: 30.289268
+            }
+        });
+</script>
